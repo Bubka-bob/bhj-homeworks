ba4b02340eb8a31eb1edf3398e5f8f782296dfda
Bubka-Bob@DESKTOP-M37FPVV <bubonia123@outlook.com>
2025-06-28 16:17:56 +0300

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,27 +1,35 @@
-      const tooltips = document.querySelectorAll('.has-tooltip');
+const tooltips = document.querySelectorAll('.has-tooltip');
 
-      tooltips.forEach(function(tooltipLink) {
-      	tooltipLink.addEventListener('click', function(e) {
-      		e.preventDefault(); // Отменяет переход по ссылке
+tooltips.forEach(function(tooltipLink) {
+	tooltipLink.addEventListener('click', function(e) {
+		e.preventDefault(); // Отменяет переход по ссылке
 
-      		// Убедимся, что ранее открытая подсказка скрылась
-      		const oldTooltip = document.querySelector('.tooltip');
-      		if (oldTooltip) {
-      			oldTooltip.parentNode.removeChild(oldTooltip);
-      		}
+		// Убедимся, что ранее открытая подсказка скрылась
+		const oldTooltip = document.querySelector('.tooltip');
+		if (oldTooltip) {
+			// Если подсказка уже открыта, проверяем, совпадает ли текст
+			if (oldTooltip.textContent === tooltipLink.title) {
+				// Если текст совпадает, инвертируем отображение подсказки
+				oldTooltip.classList.toggle('tooltip_active');
+				return;
+			} else {
+				// Если текст другой, удаляем старую подсказку
+				oldTooltip.parentNode.removeChild(oldTooltip);
+			}
+		}
 
 
-      		let tooltip = document.createElement('div');
-      		tooltip.classList.add('tooltip');
-      		tooltip.textContent = tooltipLink.title; // Показываем содержимое атрибута title
-      		document.body.appendChild(tooltip);
+		let tooltip = document.createElement('div');
+		tooltip.classList.add('tooltip');
+		tooltip.textContent = tooltipLink.title; // Показываем содержимое атрибута title
+		document.body.appendChild(tooltip);
 
-      		// Определяем позицию подсказки относительно родительского контейнера
-      		let linkRect = tooltipLink.getBoundingClientRect();
-      		tooltip.style.left = `${linkRect.left + window.scrollX}px`;
-      		tooltip.style.top = `${linkRect.bottom + window.scrollY}px`;
+		// Определяем позицию подсказки относительно родительского контейнера
+		let linkRect = tooltipLink.getBoundingClientRect();
+		tooltip.style.left = `${linkRect.left + window.scrollX}px`;
+		tooltip.style.top = `${linkRect.bottom + window.scrollY}px`;
 
-      		tooltip.classList.add('tooltip_active');
+		tooltip.classList.add('tooltip_active');
 
-      	});
-      });
+	});
+});
